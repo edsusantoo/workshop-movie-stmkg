0a835b03abb909b45aa4cbc795ffcdf0d519afda
Edi Susanto <edsusantoo@gmail.com>
2019-04-21 14:49:05 +0700

    membuat service retrofit dan confignya

--- app/src/main/java/com/edsusantoo/bismillah/moviestmkg/network/RetrofitConfig.java
+++ app/src/main/java/com/edsusantoo/bismillah/moviestmkg/network/RetrofitConfig.java
@@ -1,5 +1,19 @@
 package com.edsusantoo.bismillah.moviestmkg.network;
 
+import retrofit2.Retrofit;
+
 public class RetrofitConfig {
+
+    private static Retrofit getRetrofit() {
+        return new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/")
+                .build();
+    }
+
+    public static ApiServices getApiServices() {
+
+        return getRetrofit().create(ApiServices.class);
+    }
+
 
 }

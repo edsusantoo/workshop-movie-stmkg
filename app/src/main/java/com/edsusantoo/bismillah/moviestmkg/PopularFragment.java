b3d3b97772a0d02d5ea7e83c866dcd2ef31e51be
Edi Susanto <edsusantoo@gmail.com>
2019-04-21 14:49:58 +0700

    set item recycleview

--- app/src/main/java/com/edsusantoo/bismillah/moviestmkg/PopularFragment.java
+++ app/src/main/java/com/edsusantoo/bismillah/moviestmkg/PopularFragment.java
@@ -3,16 +3,27 @@ package com.edsusantoo.bismillah.moviestmkg;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.edsusantoo.bismillah.moviestmkg.model.ResultsItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PopularFragment extends Fragment {
+
+    RecyclerView rvPopularMovie;
+    List<ResultsItem> listData = new ArrayList<>();
 
 
     public PopularFragment() {
@@ -27,5 +38,11 @@ public class PopularFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_popular, container, false);
     }
 
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPopularMovie = view.findViewById(R.id.rv_popular_movie);
+        rvPopularMovie.setAdapter(new MovieAdapter(getActivity(), listData));
+        rvPopularMovie.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+    }
 }
